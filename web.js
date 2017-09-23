84fcc956fd09625a0e30f9187794edbbec4e3445
peakchase <ambition0917@naver.com>
2017-09-23 16:09:42 +0900

    test
    Signed-off-by: peakchase <ambition0917@naver.com>

--- web.js
+++ web.js
@@ -1,14 +1,6 @@
-var port = process.env.port || 8001;
-
-app.listen(port, function () {
-           console.log("listening on " + port);
-           });
-
-var pool= mysql.createPool(client);
-function keepAlive(){
-    pool.getConnection(function(err, connection){
-                       if(err) { return; }
-                       connection.release();
-                       });
-}
-setInterval(keepAlive, 60*1000);
+var http = require('http');
+http.createServer(function (req,res){
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.end('hello World');
+}).listen(8001);
+console.log('Server running at Cafe24');
