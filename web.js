8eedf02dd1a071ecf02a44d347fc97e91831ca8d
peakchase <ambition0917@naver.com>
2017-09-23 16:15:00 +0900

    test

--- web.js
+++ web.js
@@ -1,6 +1,57 @@
+
+var app = require('../app');
 var http = require('http');
-http.createServer(function (req,res){
-res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('hello World');
-}).listen(8001);
-console.log('Server running at Cafe24');
+var debug = require('debug')('chase:server');
+var port = process.env.port || 8001;
+
+app.listen(port, function () {
+           console.log("listening on " + port);
+           });
+app.set('port', port);
+
+var server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    
+    var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+    
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
+
+
+var pool= mysql.createPool(client);
+function keepAlive(){
+    pool.getConnection(function(err, connection){
+                       if(err) { return; }
+                       connection.release();
+                       });
+}
+setInterval(keepAlive, 60*1000);
