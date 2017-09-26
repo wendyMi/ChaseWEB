493378d81694ac47d7c391a809ca51babaefd173
peakchase <ambition0917@naver.com>
2017-09-26 19:17:01 +0900

    test

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ var users = require('./routes/users');
 var login = require('./routes/login');
 var join = require('./routes/join');
 var main = require('./routes/main');
+var qna=require('./routes/qna');
+var qna_board=require('./routes/qna_board');
+var fnq=require('./routes/fnq');
+var fnq_board=require('./routes/fnq_board');
+var material=require('./routes/material');
+var material_board=require('./routes/material_board');
+var mypage=require('./routes/mypage');
+var student=require('./routes/student');
+var user_info=require('./routes/user_info');
+var myquestion=require('./routes/myquestion');
+var mail=require('./routes/mail');
+var send=require('./routes/send');
+var read=require('./routes/read');
 
 var app = express();
 
@@ -40,6 +53,34 @@ app.get('/login',login);
 app.post('/login',login);
 
 app.get('/main',main);
+
+app.get('/material/:stage/:page',material);
+app.get('/material_board',material_board);
+app.post('/material_board',material_board);
+
+app.get('/qna/:page',qna);
+app.get('/qna_board',qna_board);
+app.post('/qna_board',qna_board);
+
+app.get('/fnq/:page',fnq);
+app.get('/fnq_board',fnq_board);
+app.post('/fnq_board',fnq_board);
+
+app.get('/mypage',mypage);
+
+app.get('/student/:page',student);
+app.post('/student/:page',student);
+
+app.get('/user_info',user_info);
+app.post('/user_info',user_info);
+
+app.get('/myquestion/:page',myquestion);
+
+app.get('/mail/:page',mail);
+app.get('/send',send);
+app.post('/send',send);
+
+app.get('/read',read);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
