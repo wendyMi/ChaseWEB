55b84a82186f8a082c91b395501cb2c882a1f9a7
peakchase <ambition0917@naver.com>
2017-09-26 13:01:50 +0900

    test
    Signed-off-by: peakchase <ambition0917@naver.com>

--- routes/join.js
+++ routes/join.js
@@ -14,7 +14,7 @@ router.post('/join',function(req,res){
     console.log("POSTING");
             
     var connection=mysql.createConnection({
-        host: 'localhost',
+        host: 10.0.0.1,
         user: 'peakchase',
         password :'wendy0917',
         database : 'peakchase'
