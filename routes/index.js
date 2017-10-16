a623b6d694317c77713237f2396677e06cff1cfc
peakchase <ambition0917@naver.com>
2017-10-16 17:50:23 +0900

    Signed-off-by: peakchase <ambition0917@naver.com>

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+           global.id="";
+           console.log("global id : "+global.id);
   res.render('index');
 });
 
